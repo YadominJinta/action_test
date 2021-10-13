35cab3ae6f2447cd8811de0fd92ae47305f39071
Yadomin <i@yadom.in>
2021-10-13 11:26:47 +0800

    feat: add go test (#2)
    * feat: Add unit test
    
    * feat: Add go test in ci
    
    * fix: Hello should return empty when name is empty

--- main.go
+++ main.go
@@ -2,33 +2,15 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
+func Hello(name string) string {
+	if name == "" {
+		return ""
+	}
+	return fmt.Sprintf("Hello, %s", name)
+}
+
 func main() {
-	wg := &sync.WaitGroup{}
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(a int) {
-			fmt.Printf("without lock %d \n", a)
-			wg.Done()
-		}(i)
-	}
-
-	wg.Wait()
-
-	mutex := &sync.Mutex{}
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		mutex.Lock()
-		go func(a int) {
-
-			defer mutex.Unlock()
-			fmt.Printf("with lock %d \n", a)
-			wg.Done()
-		}(i)
-	}
-
-	wg.Wait()
+	fmt.Println(Hello("Yadomin"))
 }
